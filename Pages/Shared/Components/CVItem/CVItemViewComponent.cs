02c4d31650159632c060dacf29f2f6394f6abd13
Philipp Schiweck <philipp.schiweck@ooglemail.com>
2021-05-04 12:14:16 +0200

    Add CVItem ViewComponent structure

---
+++ Pages/Shared/Components/CVItem/CVItemViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Website_Bewerbung.Pages.Shared.Components.CVItem
+{
+    public class CVItemViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("CVItem");
+        }
+    }
+}
